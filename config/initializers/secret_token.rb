cdd41e1c21da37a0d4f67b9b65dd03a55d891baa
Michael Scriven <bigscriv03@gmail.com>
2014-07-01 23:05:43 +0000

    initial commit of new Bond project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bond::Application.config.secret_key_base = '5a3136deee61f94cd31fc3119c3676acda457f5ae20ae659a1d7d15b3d0a6c655d1129a619ad343bce953ef437eed764fedb0af67b4f7d0da45dbbdf70ecae4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bond::Application.config.session_store :cookie_store, key: '_Bond_session'
