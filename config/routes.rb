f50a0d77bc305532e66682cb427ed46ffb9b6f9b
Michael Scriven <bigscriv03@gmail.com>
2014-07-02 00:18:39 +0000

    Create Agen controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Bond::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :agents
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
